52a52e17b967935fa2d021e7900011e3aff25768
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-03-08 16:26:15 -0300

    adiciona ex 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-return undefined;
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) { return undefined; }
+  for (let i of array) {
+    if (typeof i !== 'number') { return undefined; }
+    sum += i;
+  }
+  return Math.round(sum / array.length);
+};
+console.log(average([1, 3]));
 module.exports = average;
